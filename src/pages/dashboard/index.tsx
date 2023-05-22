1fd6b06e85be3c9cfc0800ac252f90e67e0d60d8
Vinicius <gvweb.solucoes@gmail.com>
2023-05-22 15:04:58 -0300

    Dashboard remove

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -61,7 +61,7 @@ const Dashboard: NextPage<IPrivatePageProps> = (props) => {
   return (
     <>
       <Head>
-        <title>Painel Odontologia - AAPVR</title>
+        <title>Sistema de saúde</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -69,38 +69,16 @@ const Dashboard: NextPage<IPrivatePageProps> = (props) => {
       <Layout titulo="Dashboard" email={props.email} perfil={props.types} sala={props.sala}>
         <div className="card p-4 shadow">
             <div className="alert alert-primary" role="alert">
-              <h4 className="alert-heading text-start"><img src="/assets/images/bell.svg"/> Painel de Chamados Odontologia <small style={{color: 'black'}}>versão: 1.0</small></h4>
+              <h4 className="alert-heading text-start"><img src="/assets/images/bell.svg"/> Sistema de Saúde <small style={{color: 'black'}}>Versão: PROD-1</small></h4>
               <hr/>
-              <p className="mb-3">Gerencie salas, usuários, pacientes, acesso ao chamador e painel de TV.</p>  
+              <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod perferendis eaque vel, fugiat, labore veritatis deserunt officiis itaque a numquam explicabo omnis! Fugit debitis reprehenderit sequi! Ratione veritatis ex fugit..</p>  
               <p className="mb-0"><b>Dúvidas?</b></p>
               <p className="mb-0">Estamos à disposição no e-mail informatica@aapvr.com.br ou através do sistema <a href="https://solicitaweb.aapvr.com.br">solicita web.</a></p>
             </div>
         </div>
 
         <div className="card p-4 shadow">
-                <form method='POST' onSubmit={onSubmit}>
-                    <div className="form-row">
-                        <div className="form-group col-md-10 col-lg-10">
-                        <label htmlFor="Nome">Nome do Paciente</label>
-                        <input type="text" name="paciente" className="form-control" id="paciente" required/>
-                        </div>
-                    </div>  
-      
-                    <div className="form-row">                     
-                        <div className="form-group col-md-12 col-lg-4">
-                            <label htmlFor="inputState">Profissional</label>
-                            <select id="profissional_id" className="form-control" name="profissional_id" required>
-                              <option selected disabled value='' style={{ display: 'none'}}>Selecione...</option>
-                              {renderProfissionais()}
-                            </select>
-                        </div>
-                    </div>
-
-                    <div className="d-flex mt-4">
-                        <button type="reset" className="btn-personalizado-sec mr-3">Limpar</button>
-                        <button type="submit" className="btn-personalizado">Salvar</button>
-                    </div>
-            </form>
+               
         </div> 
 
 

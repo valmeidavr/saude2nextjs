fd1948fa0126bf773a484e8f6a9b6ed1a23491e3
Vinicius <gvweb.solucoes@gmail.com>
2023-05-24 17:10:32 -0300

    Update: Cadastro Pacientes

--- src/pages/pacientes/cadastro/index.tsx
+++ src/pages/pacientes/cadastro/index.tsx
@@ -9,7 +9,7 @@ import {useRouter} from 'next/router';
 import { msgResponse } from '@/util/msg';
 import { toast } from 'react-toastify';
 
-const CadastroUnidades: NextPage<IPrivatePageProps> = (props) => {
+const CadastroPacientes: NextPage<IPrivatePageProps> = (props) => {
     const router = useRouter();
 
     async function onSubmit(event: FormEvent): Promise<void> {
@@ -31,7 +31,7 @@ const CadastroUnidades: NextPage<IPrivatePageProps> = (props) => {
   return (
     <>
       <Head>
-        <title>Sistema de Saúde</title>
+        <title>Sistema de Serviços</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -40,92 +40,92 @@ const CadastroUnidades: NextPage<IPrivatePageProps> = (props) => {
       <div className="card p-4 shadow">
                 <form method='POST' onSubmit={onSubmit}>
                     <div className="form-row">
-                        <div className="form-group col-md-10 col-lg-10">
-                        <label htmlFor="Nome">Nome do Paciente</label>
-                        <input type="text" name="name" className="form-control" id="name" required/>
+                        <div className="form-group col-md-8 col-lg-8">
+                          <label htmlFor="Nome">Nome do Paciente</label>
+                          <input type="text" name="name" className="form-control" id="name" required/>
                         </div>
-                    </div>  
-
-                    <div className="form-row">
-                        <div className="form-group col-md-12 col-lg-6">
-                        <label htmlFor="DataNasc">Data de Nascimento</label>
-                        <input type="date" name="DataNasc" className="form-control" id="DataNascPac" required/>
-                        </div>
-                    </div>
-                    <div className="row">
-                      <div className="cocol-md-12 col-lg-8l">
-                          <div className="form-group col-md-12 col-lg-9">
-                            <label htmlFor="RG">RG</label>
-                            <input type="number" name="RG" className="form-control" id="RG" required/>
-                          </div>
-                      </div>
-                      <div className="form-row">
-                          <div className="form-group col-md-12 col-lg-11">
-                            <label htmlFor="CPF">CPF</label>
-                            <input type="number" name="CPF" className="form-control" id="CPF" required/>
-                          </div>
-                      </div>
-                    </div>
-                    
-                    <div className="form-row">
-                        <div className="form-group col-md-12 col-lg-2">
-                        <label htmlFor="CEP">CEP</label>
-                        <input type="number" name="CEP" className="form-control" id="cep" required/>
+                        <div className="form-group col-md-4 col-lg-4">
+                            <label htmlFor="Email">E-mail</label>
+                            <input type="email" name="email" className="form-control" id="email" required/>
                         </div>
                     </div> 
                     <div className="form-row">
-                        <div className="form-group col-md-12 col-lg-10">
-                          <label htmlFor="Lougradouro">Logradouro</label>
-                          <input type="text" name="lougradouro" className="form-control" id="lougradouro" required/>
+                          <div className="form-group col-md-6 col-lg-6">
+                            <label htmlFor="Nome da Mãe">Nome da Mãe</label>
+                            <input type="nomeMae" name="nomeMae" className="form-control" id="nomeMae" required/>
+                          </div>
+                          <div className="form-group col-md-6 col-lg-6">
+                            <label htmlFor="Nome do Pai">Nome do Pai</label>
+                            <input type="nomePai" name="nomePai" className="form-control" id="nomePai" required/>
+                          </div>
+                        </div> 
+
+                    <div className="form-row">
+                        <div className="form-group col-md-6 col-lg-3">
+                          <label htmlFor="dataNasc">Nascimento</label>
+                          <input type="date" name="dataNasc" className="form-control" id="dataNasc" required/>
+                        </div>
+
+                        <div className="form-group col-md-6 col-lg-3">
+                            <label htmlFor="CPF">CPF</label>
+                            <input type="number" name="cpf" className="form-control" id="cpf" required/>
+                        </div>
+
+                        <div className="form-group col-md-6 col-lg-3">
+                            <label htmlFor="RG">RG</label>
+                            <input type="number" name="rg" className="form-control" id="rg" required/>
+                        </div>
+
+                        <div className="form-group col-md-6 col-lg-1">
+                            <label htmlFor="orgaoEmissor">Emissor</label>
+                            <input type="text" name="orgaoEmissor" className="form-control" id="orgaoEmissor" required/>
+                        </div>
+
+                        <div className="form-group col-md-6 col-lg-2">
+                            <label htmlFor="dtExpedicao">Expedição</label>
+                            <input type="date" name="dtExpedicao" className="form-control" id="dtExpedicao" required/>
                         </div>
                     </div>
                     <div className="form-row">
-                      <div className="form-row">
-                          <div className="form-group col-md-12 col-lg-10">
+                        <div className="form-group col-md-4 col-lg-2">
+                          <label htmlFor="CEP">CEP</label>
+                         <input type="number" name="cep" className="form-control" id="cep" required/>
+                        </div>
+                        <div className="form-group col-md-8 col-lg-10">
+                          <label htmlFor="Lougradouro">Logradouro</label>
+                          <input type="text" name="lougradouro" className="form-control" id="lougradouro" required/>
+                        </div>
+                    </div> 
+                    <div className="form-row">
+                        <div className="form-group col-md-6 col-lg-6">
+                          <label htmlFor="Lougradouro">Complemento</label>
+                          <input type="text" name="lougradouro" className="form-control" id="lougradouro" required/>
+                        </div>
+                        <div className="form-group col-md-6 col-lg-6">
                             <label htmlFor="Bairro">Bairro</label>
-                            <input type="text" name="Bairro" className="form-control" id="bairro" required/>
-                          </div>
-                      </div>
-                      <div className="form-row">
-                          <div className="form-group col-md-12 col-lg-10">
-                            <label htmlFor="Cidade">Cidade</label>
-                            <input type="text" name="Cidade" className="form-control" id="cidade" required/>
-                          </div>
-                      </div>
-                      <div className="form-row">
-                          <div className="form-group col-md-12 col-lg-6">
-                            <label htmlFor="UF">UF</label>
-                            <input type="text" name="uf" className="form-control" id="uf" required/>
-                          </div>
-                      </div>
+                            <input type="text" name="bairro" className="form-control" id="bairro" required/>
+                        </div>
                     </div>
-                     <div className="form-row">
-                          <div className="form-group col-md-12 col-lg-6">
-                            <label htmlFor="Email">E-mail</label>
-                            <input type="email" name="email" className="form-control" id="email" required/>
-                          </div>
-                      </div>
-                      <div className="form-row">
-                        <div className="form-row">
-                            <div className="form-group col-md-12 col-lg-10">
-                              <label htmlFor="Telefone">Telefone</label>
+                    <div className="form-row">
+                        <div className="form-group col-md-12 col-lg-10">
+                          <label htmlFor="Cidade">Cidade</label>
+                          <input type="text" name="Cidade" className="form-control" id="cidade" required/>
+                        </div>
+                        <div className="form-group col-md-12 col-lg-2">
+                          <label htmlFor="UF">UF</label>
+                          <input type="text" name="uf" className="form-control" id="uf" required/>
+                        </div>
+                    </div>
+                    <div className="form-row">
+                            <div className="form-group col-md-6 col-lg-6">
+                              <label htmlFor="Telefone">Telefone1</label>
                               <input type="tel" name="tel" className="form-control" id="tel" required/>
                             </div>
-                        </div>
-                          <div className="form-row">
-                              <div className="form-group col-md-12 col-lg-10">
-                                <label htmlFor="Telefone Segundario">Telefone</label>
+                            <div className="form-group col-md-6 col-lg-6">
+                                <label htmlFor="Telefone Secundario">Telefone 2</label>
                                 <input type="tel" name="tel2" className="form-control" id="tel2" required/>
                               </div>
-                          </div>
-                      </div>
-                      <div className="form-row">
-                              <div className="form-group col-md-12 col-lg-6">
-                                <label htmlFor="Nome da Mãe">Nome da Mãe</label>
-                                <input type="nome_mae" name="nome_mae" className="form-control" id="nome_mae" required/>
-                              </div>
-                          </div>
-                    
+                        </div>
                     <div className="d-flex mt-4">
                         <button type="reset" className="btn-personalizado-sec mr-3">Limpar</button>
                         <button type="submit" className="btn-personalizado">Salvar</button>
@@ -137,7 +137,7 @@ const CadastroUnidades: NextPage<IPrivatePageProps> = (props) => {
   )
 }
 
-export default CadastroUnidades;
+export default CadastroPacientes;
 
 export const getServerSideProps = withAuth(
   async (ctx: any, cookies: any, payload: any) => {
